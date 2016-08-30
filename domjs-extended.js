9a158b4c78aedd62e044d1d842d39d80c287d439
Stewie Dez <stewie.dev@gmail.com>
2016-08-30 21:51:15 +0300

    set/get

---
+++ domjs-extended.js
@@ -0,0 +1,36 @@
+(function (domjs) {
+    'use strict';
+
+    domjs.extend({
+        value: function (value, index) {
+            return this.val(value, index);
+        },
+
+        get size () {
+            return this.length;
+        },
+
+        set size (value) {
+            this.length = value;
+        },
+
+        native: function (index) {
+            return domjs.isElement(this[index]) ? this[index] : null;
+        },
+
+        disabled (value) {
+            return this.each(function (element) {
+               element.disabled = value;
+            });
+        },
+
+        lock: function () {
+            return this.find('input, button, select, textarea').disabled(true);
+        },
+
+        unlock: function () {
+            return this.find('input, button, select, textarea').disabled(false);
+        }
+    });
+
+})(app.DOM);
